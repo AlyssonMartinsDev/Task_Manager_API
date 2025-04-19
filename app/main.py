4405c89b3099d9048ef6a80025090b413ff45262
Alysson Martins <alyssonmartins.dev@gmail.com>
2025-04-18 23:39:09 -0300

    Banco de dados: Conexão com o banco de dados SQLite configurada com sucesso e operante

--- app/main.py
+++ app/main.py
@@ -1,8 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from sqlalchemy import text 
+
+from app.db.db_config import get_db
 
 app = FastAPI()
 
 
 @app.get('/')
-def read_root():
-    return {"message": "Hello, World!"}
+def infos_api(db : Session = Depends(get_db)):
+    try:
+        db.execute(text("SELECT 1"))
+        return {
+            "title": "Bem-vindo a API de Gerenciamento de tarefas",
+            "status_db": "Conectado pronto para operar."
+
+        }
+    except Exception as e:
+        return {
+            "title": "Bem-vindo a API de Gerenciamento de tarefas",
+            "status_db": "Erro ao conectar ao banco de dados.",
+            "error": str(e)
+            }  
+
